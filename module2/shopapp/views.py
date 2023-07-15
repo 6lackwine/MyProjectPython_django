5a37614b885935950624fbaa23dd3c8a7df27059
Самир Багиров <samir_261999@mail.ru>
2023-07-15 10:33:49 +0300

    Описал модели. Сгенерировал и выполнил миграции для созданных моделей. Создал связи между таблицами. Создал веб-страницы на основе Django-шаблонов. Вывел на страницу сущности из базы данных.

--- module2/shopapp/views.py
+++ module2/shopapp/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from django.contrib.auth.models import Group
 
+from .models import Product
+from.models import Order
+
 def shop_index(request: HttpRequest):
     products = [
         ("Laptop", 1999),
@@ -20,3 +23,15 @@ def groups_list(request: HttpRequest):
         "groups": Group.objects.prefetch_related("permissions").all(),
     }
     return render(request, "shopapp/groups-list.html", context=context)
+
+def products_list(request: HttpRequest):
+    context = {
+        "products": Product.objects.all(),
+    }
+    return render(request, "shopapp/products-list.html", context=context)
+
+def orders_list(request: HttpRequest):
+    context = {
+        "orders": Order.objects.select_related("user").prefetch_related("products").all(),
+    }
+    return render(request, "shopapp/orders-list.html", context=context)
