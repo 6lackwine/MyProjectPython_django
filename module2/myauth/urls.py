ff3fce1fdc0b2d29233d8739b3bd45f755e9ba6d
Самир Багиров <samir_261999@mail.ru>
2023-07-23 21:39:40 +0300

    Создал новое приложение myauth и подключил его в настройках. Создал файл urls.py в приложении myauth и подключил к корневому URLs в проекте. В приложении myauth подключил к URLs стандартные class-based views для входа и выхода пользователя, которые предоставляет Django: подключил класс LoginView в URLs и указал через as_view: имя шаблона; переадресацию аутентифицированного пользователя; доработал класс LogoutView, чтобы изменить страницу для перенаправления пользователя. Создал view для чтения данных из cookies пользователя. Создал view для установки данных в cookies пользователя. Создал view для чтения данных из session пользователя. Создал view для установки данных в session пользователя. Указал имя (name) для всех views в URLs.

---
+++ module2/myauth/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView
+from .views import get_cookie_view, set_cookie_view, set_session_view, get_session_view, logout_view, MyLogoutView
+
+app_name = "myauth"
+
+urlpatterns = [
+    path(
+        "login/",
+        LoginView.as_view(
+            template_name="myauth/login.html",
+            redirect_authenticated_user = True, # Если юзер уже прошел аутентификацию, то ему не нужно ее проходить снова и его направят на нужный адрес
+
+        ),
+        name="login"),
+    path("logout/", MyLogoutView.as_view(), name="logout"),
+    path("cookie/get/", get_cookie_view, name="cookie-get"),
+    path("cookie/set/", set_cookie_view, name="cookie-get"),
+
+    path("session/get/", get_session_view, name="session-get"),
+    path("session/set/", set_session_view, name="session-set"),
+]
